a51f26b38b28b513251a47922cb183c0c018a372
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-26 16:46:43 -0600

    added code that generates readme and initializes

--- index.js
+++ index.js
@@ -4,13 +4,40 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // title of my project and sections entitled Description, 
+    // Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of the project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please give a breif description of the project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install this project?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile('README.md', generateMarkdown(data), (err) => 
+    err ? console.error(err) : console.log("Successfully Generated!"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log("answers", answers)
+
+        writeToFile("README.md", generateMarkdown({...answers}) )
+    })
+}
 
 // Function call to initialize app
 init();
