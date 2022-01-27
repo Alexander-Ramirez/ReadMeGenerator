8f5335c22190a1f6ba1440f9cc2616b229e134df
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-26 19:52:12 -0600

    added license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "Coffeeware") {
+    return `[Coffeeware](https://github.com/Sonic853/coffeeware-license)`
+  } else if (license === "Apache License") {
+    return `[Apache](http://www.apache.org/licenses/LICENSE-2.0)`
+  } else if (license === "MIT") {
+    return`[MIT](https://mit-license.org/)`
+  } else {
+    return ""
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
